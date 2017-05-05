d4e7220242d112cbf0580bc2cc7d96a2e2ddb782
App Academy Student <student@appacademy.io>
2017-05-05 12:16:57 -0700

    Implement Clock widget

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -4,15 +4,32 @@ class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = { time: new Date() };
+
+    this.tick = this.tick.bind(this);
   }
 
   tick() {
     this.setState({ time: new Date() });
   }
 
+  componentDidMount() {
+    this.intervalCounter = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalCounter);
+  }
+
   render() {
+    const [hours, minutes, seconds] = [
+      this.state.time.getHours(),
+      this.state.time.getMinutes(),
+      this.state.time.getSeconds()
+    ];
     return (
-      <h1>Clock Widget</h1>
+      <div>
+        {hours}:{minutes}:{seconds}
+      </div>
     );
   }
 }
